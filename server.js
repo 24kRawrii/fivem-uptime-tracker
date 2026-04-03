824018e2343e450ff0b36fc2cb30f8cb80b5bc41
24kRawrii <jrmcolvi@gmail.com>
2026-04-03 14:41:33 +1100

    added cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const cors = require("cors");
 const express = require("express");
 const fetch = require("node-fetch");
 const fs = require("fs");
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 // 🔴 CHANGE THIS
